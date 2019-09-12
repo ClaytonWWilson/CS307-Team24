c716d47254850b7673f495a6a87c22282b15fc44
Leon Liang <liang268@purdue.edu>
2019-09-12 16:23:45 -0400

    Added functions Helloworld and getUsername for testing

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
+
+exports.getUsername = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('users').get()
+    .then(data => {
+        let users = [];
+        data.forEach(doc => {
+            users.push(doc.data());
+        })
+        return res.json(users);
+    })
+    .catch((err) => console.error(err));
+})
