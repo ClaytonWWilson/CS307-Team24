2dc89b99b652b62f3196587871d4d9b0c269db18
Clayton Wilson <claytonwilson@protonmail.com>
2019-09-29 23:31:33 -0400

    Added edit page.

---
+++ twistter-frontend/src/pages/edit.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class edit extends Component {
+    render() {
+        return (
+            <div>
+                <b>yo, this the edit profile page</b>
+            </div>
+        )
+    }
+}
+
+export default edit;
