29c07a9b144dd8f16da38de83d93263cf08774b7
Danny Voltz <drlvstar5@gmail.com>
2019-10-02 16:24:01 -0500

    commit

--- twistter-frontend/src/App.js
+++ twistter-frontend/src/App.js
@@ -5,16 +5,16 @@ import './App.css';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
+
 import NavBar, { Navbar } from './components/layout/NavBar';
 
-// Pages
 import home from './Home.js';
 import register from './Register.js';
 import login from './Login.js';
 import user from './pages/user';
 import writeMicroblog from './Writing_Microblogs.js';
-import edit from './pages/edit.js';
 import userLine from './Userline.js';
+import logout from './Logout.js'
 
 class App extends Component {
   render() {
@@ -30,8 +30,8 @@ class App extends Component {
           <Route exact path="/login" component={login}/>
           <Route exact path="/user" component={user}/>
           <Route exact path="/home" component={writeMicroblog}/>
-          <Route exact path="/edit" component={edit}/>
           <Route exact path="/userline" component={userLine}/>
+	  <Route exact path="/logout" component={logout}/>
         </div>
 
       </Router>
