f1e5c584e1442aad00ae7cf01b0c1bb5473515fe
Danny Voltz <drlvstar5@gmail.com>
2019-10-03 12:00:58 -0500

    added alert

--- twistter-frontend/src/Logout.js
+++ twistter-frontend/src/Logout.js
@@ -3,8 +3,17 @@ import React, { Component} from 'react';
 import './App.css';
 import logo from './images/twistter-logo.png';
 import TextField from '@material-ui/core/TextField';
+import logoutUser from '../redux/actions/userActions.js'
+import { Tooltip, IconButton } from '@material-ui/core';
+import writeMicroblog from './Writing_Microblogs.js';
+import PropTypes from "prop-types";
 
 class Logout extends Component {
+  handleLogout = () => {
+    localStorage.removeItem("FBIdToken");
+    alert("Successfully Logged Out");
+    this.props.history.push('/');
+  };
   render() {
     return(
       <div>
@@ -13,10 +22,18 @@ class Logout extends Component {
 	    <b>Logout of your Twistter Account</b>
       <br/><br/>
       <br/><br/>
-	    <button className="authButtons register" type="submit">Sign Out</button>
+	    <button className="authButtons register" onClick={this.handleLogout} type="submit">Sign Out</button>
       </div>
     );
   };
+}
+Logout.protoTypes = {
+  classes: PropTypes.object.isRequired,
+  logoutUser: PropTypes.object.isRequired
+};
+
+const mapActionsToProps = {
+  logoutUser
 }
 
 export default Logout;
