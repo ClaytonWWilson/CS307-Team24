00ed9d64c59226845b7c3ca40fe01de143df5915
Clayton Wilson <claytonwilson@protonmail.com>
2019-10-10 14:13:04 -0400

    store.js

--- twistter-frontend/src/redux/store.js
+++ twistter-frontend/src/redux/store.js
@@ -15,12 +15,13 @@ const reducers = combineReducers({
   UI: uiReducer
 });
 
+
 const store = createStore(
   reducers,
   initialState,
   compose(
     applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() // Can be removed after debugging is finished
   )
 );
 
