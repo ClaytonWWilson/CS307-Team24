4aa029c649c46b265697471b534fad186e1f8080
Leon Liang <liang268@purdue.edu>
2019-10-24 19:34:31 -0400

    modified getUserDetails

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -178,37 +178,15 @@ exports.updateProfileInfo = (req, res) => {
 
 exports.getUserDetails = (req, res) => {
   let userData = {};
-  db.doc(`/users/${req.params.handle}`)
+  db.doc(`/users/${req.body.handle}`)
     .get()
     .then((doc) => {
       if (doc.exists) {
-        userData.user = doc.data();
-        return db
-          .collection("post")
-          .where("userHandle", "==", req.params.handle)
-          .orderBy("createdAt", "desc")
-          .get();
-      } else {
-        return res.status(404).json({
-          error: "User not found"
-        });
-      }
-    })
-    .then((data) => {
-      userData.posts = [];
-      data.forEach((doc) => {
-        userData.posts.push({
-          body: doc.data().body,
-          createAt: doc.data().createAt,
-          userHandle: doc.data().userHandle,
-          userImage: doc.data().userImage,
-          likeCount: doc.data().likeCount,
-          commentCount: doc.data().commentCount,
-          postId: doc.id
-        });
-      });
-      return res.json(userData);
-    })
+        userData = doc.data();
+        return res.status(200).json({userData});
+    } else {
+      return res.status(400).json({error: "User not found."})
+    }})
     .catch((err) => {
       console.error(err);
       return res.status(500).json({ error: err.code });
