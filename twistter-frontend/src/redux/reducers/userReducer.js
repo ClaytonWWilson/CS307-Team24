323cb5bceefd916189a173bb931a673c3be4ec44
Aditya Sankaran <aditya.sankaran99@gmail.com>
2019-12-04 18:15:37 -0500

    Revert "module fuse and syntax errors fixed"
    This reverts commit 4a4733f593d7965e43c49a6cb41f8b95f554c05c.

--- twistter-frontend/src/redux/reducers/userReducer.js
+++ twistter-frontend/src/redux/reducers/userReducer.js
@@ -51,7 +51,6 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 ...action.payload
-            }
 
         case LOADING_USER:
             return {
